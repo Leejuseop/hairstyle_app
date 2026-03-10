f99240af82f1861c73afdecb2ae297688c8a86f2
juseop <juseop0915@gmail.com>
2026-03-10 13:02:47 +0900

    init: create spring boot project

---
+++ src/main/java/com/juseop/hair_simulator/HairSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.juseop.hair_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juseop/hair_simulator/HairSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juseop.hair_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
