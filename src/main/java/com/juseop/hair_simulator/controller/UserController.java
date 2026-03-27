598c7f20d8dc67210ba286009f968a696a9b7550
juseop <juseop0915@gmail.com>
2026-03-27 18:05:25 +0900

    feat: UserController-user registration mapping

--- src/main/java/com/juseop/hair_simulator/controller/UserController.java
+++ src/main/java/com/juseop/hair_simulator/controller/UserController.java
@@ -1,14 +1,40 @@
 package com.juseop.hair_simulator.controller;
 
+import com.juseop.hair_simulator.domain.User;
+import com.juseop.hair_simulator.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+
+    private final UserRepository userRepository;
+
     @GetMapping("/join")
     public String joinPage(){
-        return "join.html";
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinService(@RequestParam("userId") String userId,
+                              @RequestParam("userPassword") String userPassword,
+                              @RequestParam("userName") String userName,
+                              RedirectAttributes rttr){
+
+        User user = new User(userId,userPassword, userName);
+        userRepository.save(user);
+
+        rttr.addFlashAttribute("message", "회원가입이 완료되었습니다.");
+
+        return "redirect:/";
     }
 }
