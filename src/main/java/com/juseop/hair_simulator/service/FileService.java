ca652787c806034452d63d94233a65ad067bed93
juseop <juseop0915@gmail.com>
2026-03-31 10:23:30 +0900

    feat: FileService

---
+++ src/main/java/com/juseop/hair_simulator/service/FileService.java
@@ -0,0 +1,27 @@
+package com.juseop.hair_simulator.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+public class FileService {
+    private final String uploadDir = "C:/hairstyle_app/uploads/";
+
+    public String saveFile(MultipartFile file) throws IOException{
+        File folder = new File(uploadDir);
+        if(!folder.exists()){
+            folder.mkdirs();
+        }
+
+        String originalFileName = file.getOriginalFilename();
+        String uuid = UUID.randomUUID().toString();
+        String savedName = originalFileName + uuid;
+
+        File savedPath = new File(uploadDir + savedName);
+        file.transferTo(savedPath);
+
+        return savedName;
+    }
+}
