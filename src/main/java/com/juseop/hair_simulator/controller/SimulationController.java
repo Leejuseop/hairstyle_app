eca4442b24953c3b0f5809e7cd42a894af8569d1
juseop <juseop0915@gmail.com>
2026-03-31 11:36:08 +0900

    feat: SimulationController

---
+++ src/main/java/com/juseop/hair_simulator/controller/SimulationController.java
@@ -0,0 +1,43 @@
+package com.juseop.hair_simulator.controller;
+
+import com.juseop.hair_simulator.service.FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+
+@Controller
+@RequiredArgsConstructor
+public class SimulationController {
+
+    private final FileService fileService;
+
+    @GetMapping("/main")
+    public String mainPage() {
+        return "main";
+    }
+
+    @PostMapping("/main")
+    public String main(@RequestParam("imageFile") MultipartFile file,
+                       RedirectAttributes rttr) {
+
+        if (file.isEmpty()) {
+            rttr.addFlashAttribute("msg", "업로드된 사진이 없습니다. 다시 선택해주세요!");
+            return "redirect:/main";
+        }
+
+        try {
+            String saveFileName = fileService.saveFile(file);
+            rttr.addAttribute("fileName", saveFileName);
+            return "redirect:/simulate";
+        } catch (IOException e) {
+            rttr.addFlashAttribute("msg", "파일 저장 중 오류가 발생했습니다.");
+            return "redirect:/main";
+        }
+    }
+}
