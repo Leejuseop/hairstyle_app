2fce15cd4cd9dd8cbccdbff703c6bc26b1daa9e7
juseop <juseop0915@gmail.com>
2026-04-08 15:09:39 +0900

    feat: UserKeyword - domain for keyword DB

---
+++ src/main/java/com/juseop/hair_simulator/domain/UserKeyword.java
@@ -0,0 +1,32 @@
+package com.juseop.hair_simulator.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+public class UserKeyword {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pkId;
+
+    private String category;
+    private String keyWord;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "history_id")
+    private UserHistory userHistory;
+
+    public UserKeyword(String category, String keyWord, User user, UserHistory userHistory) {
+        this.category = category;
+        this.keyWord = keyWord;
+        this.user = user;
+        this.userHistory = userHistory;
+    }
+}
