a7d6ccdddbb7b5ad3b1248c19f8f2325925a546b
juseop <juseop0915@gmail.com>
2026-04-08 15:16:38 +0900

    feat: UserKeywordRepository

---
+++ src/main/java/com/juseop/hair_simulator/repository/UserKeywordRepository.java
@@ -0,0 +1,13 @@
+package com.juseop.hair_simulator.repository;
+
+import com.juseop.hair_simulator.domain.User;
+import com.juseop.hair_simulator.domain.UserKeyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserKeywordRepository extends JpaRepository<UserKeyword, Long> {
+    public List<UserKeyword> findByUser(User user);
+}
